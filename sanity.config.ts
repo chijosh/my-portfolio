3c938807b97973f000d541d48e94f903c47d5d06
Chi Joshua <chijosh2010@hotmail.com>
2025-04-09 21:51:55 +0200

    MyPortfolia-05_ReplacedPdf

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,11 +3,17 @@ import { structureTool } from "sanity/structure";
 import schemas from "./sanity/schemas";
 import { visionTool } from "@sanity/vision";
 
+// const projectId = process.env.SANITY_STUDIO_ID
+// const dataset = process.env.SANITY_STUDIO_DATASET
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const dataset = process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET
+
 const config = defineConfig({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-  dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET as string,
+  projectId: projectId as string,
+  dataset: dataset as string,
   title: "Portfolio Website",
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_PROJECT_API_VERSION as string,
+  apiVersion: process.env.SANITY_STUDIO_API_VERSION as string,
   basePath: "/studio",
   plugins: isDev ? [structureTool(), visionTool()] : [structureTool()],
   schema: { types: schemas },
