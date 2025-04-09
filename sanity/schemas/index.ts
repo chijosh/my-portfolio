f96ddec4c7d5353b6ebf714163dc9b5097ac24cd
Chi Joshua <chijosh2010@hotmail.com>
2025-04-09 14:00:20 +0200

    MyPortfolia-02_GiveFleshAndBones

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,17 @@
+import { Experiences } from "./Experience";
+import { FAQ } from "./FAQ";
+import { Projects } from "./Projects";
+import { Services } from "./Services";
+import { Technologies } from "./Technologies";
+import { Testimonials } from "./Testimonials";
+
+const schemas = [
+  Technologies,
+  Services,
+  Experiences,
+  FAQ,
+  Testimonials,
+  Projects,
+];
+
+export default schemas;
---
+++ src/app/sitemap.ts
@@ -0,0 +1,40 @@
+import { MetadataRoute } from "next";
+import { getProjects } from "../../sanity/sanity-utils";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const projects = await getProjects();
+
+  // static pages
+  const staticPages: MetadataRoute.Sitemap = [
+    {
+      url: process.env.NEXT_PUBLIC_LIVE_URL as string,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+
+    {
+      url: `${process.env.NEXT_PUBLIC_LIVE_URL}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+
+    {
+      url: `${process.env.NEXT_PUBLIC_LIVE_URL}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+
+  // dynamic project pages
+  const projectPages: MetadataRoute.Sitemap = projects.map((project) => ({
+    url: `${process.env.NEXT_PUBLIC_LIVE_URL}/projects/${project.slug}`,
+    lastModified: new Date(project._createdAt),
+    changeFrequency: "weekly",
+    priority: 0.7,
+  }));
+
+  return [...staticPages, ...projectPages];
+}
